8a5743afd192bfeaa979a66f3903fc2a726b10bc
Myrellatristao <125603254+Myrellatristao@users.noreply.github.com>
2024-10-02 13:58:05 -0300

    02/10/24

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,5 +1,59 @@
 import { getCSS, tickConfig } from "./common.js"
+
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubbusercontent.com/'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadedeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadedeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
     
+    const laytout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuarios',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                size: 30,
+                font: getCSS('--font')
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            tittle: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhoes de Usuarios ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, laytout)
 }
+
+quantidadeUsuariosPorRede()
